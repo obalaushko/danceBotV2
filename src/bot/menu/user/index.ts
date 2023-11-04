3be004fe4702769eb531d6acf142481e1a615839
Deimos <alex279368@gmail.com>
2023-11-04 22:50:58 +0200

    Add notifications menu

--- src/bot/menu/user/index.ts
+++ src/bot/menu/user/index.ts
@@ -1,11 +1,18 @@
 import { Menu } from '@grammyjs/menu';
 import { MSG } from '../../../constants';
 import { backToUserMain } from './backToMainMenu';
-import { getUserWithSubscriptionById } from '../../../mongodb/operations';
+import {
+    getUserById,
+    getUserWithSubscriptionById,
+} from '../../../mongodb/operations';
+import { notificationsMenu } from './notificationsMenu';
+import { LOGGER } from '../../../logger';
 
 export const userMenu = new Menu('user')
     .text(MSG.buttons.user.showSubscription, async (ctx) => {
-        const { user: {id} } = await ctx.getAuthor();
+        const {
+            user: { id },
+        } = await ctx.getAuthor();
 
         const user = await getUserWithSubscriptionById(id);
 
@@ -16,12 +23,24 @@ export const userMenu = new Menu('user')
             await ctx.editMessageText(MSG.errors.unknownError);
         }
     })
-    .text(MSG.buttons.user.notifications)
+    .text(MSG.buttons.user.notifications, async (ctx) => {
+        const {
+            user: { id },
+        } = await ctx.getAuthor();
+        try {
+            const user = await getUserById(id);
+
+            ctx.menu.nav('notificationsMenu');
+            await ctx.editMessageText(MSG.user.notification.main(user));
+        } catch (err) {
+            LOGGER.error('[NotificationsMenu][error]', { metadata: { err } });
+        }
+    })
     .row()
     .text(MSG.buttons.user.paymentDetails, async (ctx) => {
-        
         ctx.menu.nav('backToUserMain');
         await ctx.editMessageText(MSG.payments.static);
     });
 
 userMenu.register(backToUserMain);
+userMenu.register(notificationsMenu);
