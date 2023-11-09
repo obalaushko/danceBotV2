4e3c972d9a754cce60c88e6b0377b8f3d897a062
Deimos <alex279368@gmail.com>
2023-11-09 16:31:43 +0200

    Disabled telegram logger

--- src/logger/winston.ts
+++ src/logger/winston.ts
@@ -1,38 +1,38 @@
 import { createLogger, format, transports } from 'winston';
-import TelegramLogger from 'winston-telegram';
+// import TelegramLogger from 'winston-telegram';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const LOGGER_BOT_TOKEN = process.env.LOGGER_BOT_TOKEN || '';
-const mode = process.env.NODE_ENV || 'development';
+// const LOGGER_BOT_TOKEN = process.env.LOGGER_BOT_TOKEN || '';
+// const mode = process.env.NODE_ENV || 'development';
 
 const { combine, timestamp, json, errors } = format;
 const errorsFormat = errors({ stack: true });
 
-const telegramTransport = new TelegramLogger({
-    token: LOGGER_BOT_TOKEN,
-    chatId: -4022952026, //https://api.telegram.org/<BOT_TOKEN>/getUpdates
-    disableNotification: true,
-    batchingDelay: 1000,
-    parseMode: 'HTML',
-    formatMessage: (info) => {
-        try {
-            return `<b>[${info.level}]</b> ${info.message}: <pre>${
-                info.metadata ? JSON.stringify(info.metadata) : ''
-            }</pre>`;
-        } catch (err) {
-            console.error(`[error] ${err}`);
-            return `[${info.level}] ${err}`;
-        }
-    },
-});
+// const telegramTransport = new TelegramLogger({
+//     token: LOGGER_BOT_TOKEN,
+//     chatId: -4022952026, //https://api.telegram.org/<BOT_TOKEN>/getUpdates
+//     disableNotification: true,
+//     batchingDelay: 1000,
+//     parseMode: 'HTML',
+//     formatMessage: (info) => {
+//         try {
+//             return `<b>[${info.level}]</b> ${info.message}: <pre>${
+//                 info.metadata ? JSON.stringify(info.metadata) : ''
+//             }</pre>`;
+//         } catch (err) {
+//             console.error(`[error] ${err}`);
+//             return `[${info.level}] ${err}`;
+//         }
+//     },
+// });
 
 const logger = createLogger();
 
-if (mode === 'production') {
-    logger.add(telegramTransport);
-}
+// if (mode === 'production') {
+//     logger.add(telegramTransport);
+// }
 
 logger.add(
     new transports.Console({
