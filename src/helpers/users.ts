59250fa28e1546cc0698ad64ecbd302bcc07bd31
Deimos <alex279368@gmail.com>
2024-02-24 19:22:35 +0200

    Minor update telegram user

--- src/helpers/users.ts
+++ src/helpers/users.ts
@@ -3,20 +3,17 @@ import { bot } from '../bot/bot.js';
 import * as dotenv from 'dotenv';
 import { LOGGER } from '../logger/index.js';
 import { UserModel } from '../mongodb/schemas/user.js';
-import { updateUsersToInactive } from '../mongodb/operations/users.js';
-import { ROLES } from '../constants/global.js';
 dotenv.config();
 
 const ENVS = process.env;
 const GROUP_ID = ENVS.GROUP_ID || '';
 //! BOT must be admin in the group
-export const removeUserFromGroup = async (
-    userIds: number[],
-    typeGroup: string = 'supergroup'
-) => {
+export const removeUserFromGroup = async (userIds: number[]) => {
+    const { type } = await bot.api.getChat(GROUP_ID);
+
     userIds.forEach(async (id) => {
         try {
-            if (typeGroup === 'supergroup') {
+            if (type === 'supergroup') {
                 await bot.api.unbanChatMember(GROUP_ID, id);
             } else {
                 await bot.api.banChatMember(GROUP_ID, id);
@@ -35,22 +32,17 @@ export const checkAndUpdateTelegramUser = async () => {
         for (const user of users) {
             const {
                 user: { id, first_name, username },
-                status,
             } = await bot.api.getChatMember(GROUP_ID, user.userId);
 
-            if (first_name && username) {
-                user.firstName = first_name;
-                user.username = username;
+            if (first_name || username) {
+                if (first_name) user.firstName = first_name;
+                if (username) user.username = username;
                 await user.save();
             } else {
-                LOGGER.error(
+                LOGGER.warn(
                     '[checkAndUpdateTelegramUser] Failed to update user',
-                    { metadata: { first_name, username } }
+                    { metadata: { userId: id, first_name, username } }
                 );
-            }
-
-            if (status === 'left' && user.role !== ROLES.Inactive) {
-                await updateUsersToInactive(id);
             }
         }
     } catch (err) {
