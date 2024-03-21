b89d4eb7dc0928aeabed3fa8c91ab566affec149
Deimos <alex279368@gmail.com>
2024-03-21 12:22:34 +0200

    Work in progress...

--- src/bot/chats/private/about.command.ts
+++ src/bot/chats/private/about.command.ts
@@ -1,4 +1,7 @@
+import { ROLES } from '../../../constants/global.js';
 import { MSG } from '../../../constants/messages.js';
+import { LOGGER } from '../../../logger/index.js';
+import { getUserById } from '../../../mongodb/operations/users.js';
 import { privateChat } from '../../bot.js';
 
 /**
@@ -8,11 +11,21 @@ import { privateChat } from '../../bot.js';
 export const aboutCommand = () => {
     privateChat.command('about', async (ctx) => {
         const {
-            user: { is_bot },
+            user: { id, is_bot },
         } = await ctx.getAuthor();
 
         if (is_bot) return;
+        try {
+            const user = await getUserById(id);
+            if (user?.role === ROLES.Admin || user?.role === ROLES.Developer) {
+                
+            } else if (user?.role === ROLES.User) {
 
-        await ctx.reply(MSG.about);
+            } else {
+                await ctx.reply(MSG.about);
+            }
+        } catch (error) {
+            LOGGER.error('[aboutCommand]', { metadata: error });
+        }
     });
 };
