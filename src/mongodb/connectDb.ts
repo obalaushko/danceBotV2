862444cf1d316babe2868ea94ef9be2e78f41cc9
Deimos <alex279368@gmail.com>
2024-09-21 14:38:27 +0300

    Fix format

--- src/mongodb/connectDb.ts
+++ src/mongodb/connectDb.ts
@@ -10,9 +10,9 @@ const HOST = ENV_VARIABLES.DB_HOST;
 
 const dbUrl = `${DB}${USER}:${PASSWORD}@${HOST}/${DB_NAME}`;
 
-let dbConnected = false;  // Database connection status
-const MAX_RETRIES = 5;    // Maximum number of connection attempts
-const RETRY_INTERVAL = 5000;  // Interval between attempts (in ms)
+let dbConnected = false; // Database connection status
+const MAX_RETRIES = 5; // Maximum number of connection attempts
+const RETRY_INTERVAL = 5000; // Interval between attempts (in ms)
 
 const connectDb = async (retryCount = 0) => {
     try {
@@ -21,8 +21,8 @@ const connectDb = async (retryCount = 0) => {
         const mongoDbConnection = await mongoose.connect(dbUrl, {
             retryReads: true,
             retryWrites: true,
-            serverSelectionTimeoutMS: 30000,  
-            socketTimeoutMS: 45000,    
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
         });
 
         if (mongoDbConnection.connection.readyState === 1) {
@@ -31,33 +31,46 @@ const connectDb = async (retryCount = 0) => {
                 metadata: '',
                 sendLog: true,
             });
-            return true;  
+            return true;
         } else {
             dbConnected = false;
             LOGGER.error(`[connectDb][DB connection failed]`, { metadata: '' });
-            return false;  
+            return false;
         }
     } catch (error: any) {
         dbConnected = false;
-        LOGGER.error(`[connectDb][DB connection failed on attempt ${retryCount + 1}]`, {
-            metadata: { error: error.message, stack: error.stack.toString() },
-        });
+        LOGGER.error(
+            `[connectDb][DB connection failed on attempt ${retryCount + 1}]`,
+            {
+                metadata: {
+                    error: error.message,
+                    stack: error.stack.toString(),
+                },
+            }
+        );
 
         if (retryCount < MAX_RETRIES) {
-            LOGGER.info(`[connectDb][Retrying in ${RETRY_INTERVAL / 1000} seconds...]`, { metadata: '' });
-
+            LOGGER.info(
+                `[connectDb][Retrying in ${RETRY_INTERVAL / 1000} seconds...]`,
+                { metadata: '' }
+            );
 
             setTimeout(() => connectDb(retryCount + 1), RETRY_INTERVAL);
         } else {
-            LOGGER.error(`[connectDb][Max retries reached, connection failed]`, {
-                metadata: { error: error.message, stack: error.stack.toString() },
-            });
+            LOGGER.error(
+                `[connectDb][Max retries reached, connection failed]`,
+                {
+                    metadata: {
+                        error: error.message,
+                        stack: error.stack.toString(),
+                    },
+                }
+            );
         }
 
-        return false; 
+        return false;
     }
 };
-
 
 const isDbConnected = () => dbConnected;
 
